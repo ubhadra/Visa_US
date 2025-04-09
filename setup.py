737df25a3cb77f444e5265e2f477d7a7654c5e29
ubhadra <bhadra.upahar@gmail.com>
2025-04-09 14:49:28 +0530

    requiremnents.txt updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Visa_US",
+    version="0.0.0",
+    author="Upahar",
+    author_email="bhadra.upahar@gmail.com",
+    packages=find_packages()
+)
